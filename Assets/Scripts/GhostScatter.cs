41367c8d81d5d6208927d2f1c42dc471184ab6f0
dhotters <57628184+dhotters@users.noreply.github.com>
2023-06-29 03:39:28 +0200

    Scatter AI to scatter targets

--- Assets/Scripts/GhostScatter.cs
+++ Assets/Scripts/GhostScatter.cs
@@ -11,28 +11,56 @@ public class GhostScatter : GhostBehavior
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //Node node = other.GetComponent<Node>();
+
+        //// NOTE frightened will overwrite this movement hence the check
+        //if (node != null && this.enabled && !this.ghost.frightened.enabled)
+        //{
+        //    // Go into random direction
+        //    int index = Random.Range(0, node.availableDirections.Count);
+
+        //    // check that the ghost does not go back to the previous node
+        //    if (node.availableDirections[index] == -this.ghost.movement.direction && node.availableDirections.Count > 1)
+        //    {
+        //        // if it does go back, negate it unless its the only direction possible
+        //        index++; // simply take the next index instead of taking a random one again
+
+        //        // correct for overflow
+        //        if (index >= node.availableDirections.Count)
+        //        {
+        //            index = 0;
+        //        }
+        //    }
+
+        //    this.ghost.movement.SetDirection(node.availableDirections[index]);
+        //}
+
+        // Get the node
         Node node = other.GetComponent<Node>();
 
-        // NOTE frightened will overwrite this movement hence the check
-        if (node != null && this.enabled && !this.ghost.frightened.enabled)
+        if (node != null && enabled && !ghost.frightened.enabled)
         {
-            // Go into random direction
-            int index = Random.Range(0, node.availableDirections.Count);
+            Vector2 direction = Vector2.zero;
+            float minDistance = float.MaxValue;
 
-            // check that the ghost does not go back to the previous node
-            if (node.availableDirections[index] == -this.ghost.movement.direction && node.availableDirections.Count > 1)
+            // we move into the direction that minimizes the distance between ghost and pacman
+            foreach (Vector2 availableDirection in node.availableDirections)
             {
-                // if it does go back, negate it unless its the only direction possible
-                index++; // simply take the next index instead of taking a random one again
+                // new pos if we would move in current availabledirection
+                Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y);
 
-                // correct for overflow
-                if (index >= node.availableDirections.Count)
+                // compute distance
+                float distance = (ghost.scatterTarget.position - newPosition).sqrMagnitude; // sqrMagnitude because its faster which is important for ML
+
+                // find direction which minizes distance
+                if (distance < minDistance)
                 {
-                    index = 0;
+                    direction = availableDirection;
+                    minDistance = distance;
                 }
             }
 
-            this.ghost.movement.SetDirection(node.availableDirections[index]);
+            this.ghost.movement.SetDirection(direction);
         }
     }
 }
