11b81539b11546c4d29838caa8f93309afa07745
dhotters <57628184+dhotters@users.noreply.github.com>
2023-06-29 03:47:37 +0200

    Added randomness to scatter when close to tgt

--- Assets/Scripts/GhostScatter.cs
+++ Assets/Scripts/GhostScatter.cs
@@ -11,52 +11,57 @@ public class GhostScatter : GhostBehavior
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //Node node = other.GetComponent<Node>();
-
-        //// NOTE frightened will overwrite this movement hence the check
-        //if (node != null && this.enabled && !this.ghost.frightened.enabled)
-        //{
-        //    // Go into random direction
-        //    int index = Random.Range(0, node.availableDirections.Count);
-
-        //    // check that the ghost does not go back to the previous node
-        //    if (node.availableDirections[index] == -this.ghost.movement.direction && node.availableDirections.Count > 1)
-        //    {
-        //        // if it does go back, negate it unless its the only direction possible
-        //        index++; // simply take the next index instead of taking a random one again
-
-        //        // correct for overflow
-        //        if (index >= node.availableDirections.Count)
-        //        {
-        //            index = 0;
-        //        }
-        //    }
-
-        //    this.ghost.movement.SetDirection(node.availableDirections[index]);
-        //}
 
         // Get the node
         Node node = other.GetComponent<Node>();
 
+
         if (node != null && enabled && !ghost.frightened.enabled)
         {
             Vector2 direction = Vector2.zero;
-            float minDistance = float.MaxValue;
 
-            // we move into the direction that minimizes the distance between ghost and pacman
-            foreach (Vector2 availableDirection in node.availableDirections)
+            // Compute distance to scatter target
+            float dist = (ghost.scatterTarget.position - ghost.transform.position).sqrMagnitude;
+
+            // if distance is smaller than 8 units, then go in random directions
+            if (dist <= 64)
             {
-                // new pos if we would move in current availabledirection
-                Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y);
+                // go into random direction
+                int index = Random.Range(0, node.availableDirections.Count);
 
-                // compute distance
-                float distance = (ghost.scatterTarget.position - newPosition).sqrMagnitude; // sqrMagnitude because its faster which is important for ML
-
-                // find direction which minizes distance
-                if (distance < minDistance)
+                // check that the ghost does not go back to the previous node
+                if (node.availableDirections[index] == -this.ghost.movement.direction && node.availableDirections.Count > 1)
                 {
-                    direction = availableDirection;
-                    minDistance = distance;
+                    // if it does go back, negate it unless its the only direction possible
+                    index++; // simply take the next index instead of taking a random one again
+
+                    // correct for overflow
+                    if (index >= node.availableDirections.Count)
+                    {
+                        index = 0;
+                    }
+                }
+                direction = node.availableDirections[index];
+
+            } else
+            {
+                float minDistance = float.MaxValue;
+
+                // we move into the direction that minimizes the distance between ghost and pacman
+                foreach (Vector2 availableDirection in node.availableDirections)
+                {
+                    // new pos if we would move in current availabledirection
+                    Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y);
+
+                    // compute distance
+                    float distance = (ghost.scatterTarget.position - newPosition).sqrMagnitude; // sqrMagnitude because its faster which is important for ML
+
+                    // find direction which minizes distance
+                    if (distance < minDistance)
+                    {
+                        direction = availableDirection;
+                        minDistance = distance;
+                    }
                 }
             }
 
